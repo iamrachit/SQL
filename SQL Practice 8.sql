d7a84da98cb1095c8f05d77aec3b4fceaba54437
Rachit S Garg <60937657+iamrachit@users.noreply.github.com>
2022-12-04 15:42:08 +0530

    Add files via upload

--- SQL Practice 8.sql
+++ SQL Practice 8.sql
@@ -1,3 +1,109 @@
 create database operation;
 use operation;
 
+create table if not exists course(
+course_id int,
+course_name varchar(50),
+course_desc varchar(60),
+course_tag varchar(50));
+
+create table if not exists student(
+student_id int,
+student_name varchar(30),
+student_mobile int,
+student_course_enroll varchar(30),
+student_course_id int);
+
+insert into course values(101 , 'fsda' , 'full stack data analytics' , 'Analytics'),
+(102 , 'fsds' , 'full stack data analytics' , 'Analytics'),
+(103 , 'fsds' , 'full stack data science' , 'DS'),
+(104 , 'big data' , 'full stack big data' , 'BD'),
+(105 , 'mern' , 'web dev' , 'mern'),
+(106 , 'blockchain' , 'full stack blockchain' , 'BC'),
+(101 , 'java' , 'full stack java' , 'java'),
+(102 , 'testing' , 'full testing ' , 'testing '),
+(105 , 'cybersecurity' , 'full stack cybersecurity' , 'cybersecurity'),
+(109 , 'c' , 'c language' , 'c'),
+(108 , 'c++' , 'C++ language' , 'language');
+
+insert into student values(301 , "sudhanshu", 3543453,'yes', 101),
+(302 , "sudhanshu", 3543453,'yes', 102),
+(301 , "sudhanshu", 3543453,'yes', 105),
+(302 , "sudhanshu", 3543453,'yes', 106),
+(303 , "sudhanshu", 3543453,'yes', 101),
+(304 , "sudhanshu", 3543453,'yes', 103),
+(305 , "sudhanshu", 3543453,'yes', 105),
+(306 , "sudhanshu", 3543453,'yes', 107),
+(306 , "sudhanshu", 3543453,'yes', 103);
+
+# Who have enrolled in a specific course 
+# For that we can perform join operation 
+
+# Inner Join
+select c.course_id, c.course_name, c.course_desc, s.student_id, s.student_name, s.student_course_id from course c
+inner join student s on c.course_id = s.student_course_id;
+
+# Left Join 
+select c.course_id, c.course_name, c.course_desc, s.student_id, s.student_name, s.student_course_id from course c
+left join student s on c.course_id = s.student_course_id;
+
+# Name of course which are not purchased w.r.t student_id
+select c.course_id, c.course_name, c.course_desc, s.student_id, s.student_name, s.student_course_id from course c
+left join student s on c.course_id = s.student_course_id where s.student_id is null;
+
+# Right Join
+select c.course_id, c.course_name, c.course_desc, s.student_id, s.student_name, s.student_course_id from course c
+right join student s on c.course_id = s.student_course_id;
+
+# Cross Join - All possible join from both the table 
+select c.course_id, c.course_name, c.course_desc, s.student_id, s.student_name, s.student_course_id from course c
+cross join student s on c.course_id = s.student_course_id;
+# Cross and Inner Join - With condition like c.course_id = s.student_course_id; the cross join will work similarly as inner join but without a condition it will give all the possible outcomes as shown below 
+select c.course_id, c.course_name, c.course_desc, s.student_id, s.student_name, s.student_course_id from course c
+cross join student s;
+
+# Indexing 
+create table if not exists course1 (
+course_id int,
+course_name varchar(50),
+course_desc varchar(60),
+course_tag varchar(50),
+index(course_id));
+
+show index from course1;
+
+insert into course1 values(101 , 'fsda' , 'full stack data analytics' , 'Analytics'),
+(102 , 'fsds' , 'full stack data analytics' , 'Analytics'),
+(103 , 'fsds' , 'full stack data science' , 'DS'),
+(104 , 'big data' , 'full stack big data' , 'BD'),
+(105 , 'mern' , 'web dev' , 'mern'),
+(106 , 'blockchain' , 'full stack blockchain' , 'BC'),
+(101 , 'java' , 'full stack java' , 'java'),
+(102 , 'testing' , 'full testing ' , 'testing '),
+(105 , 'cybersecurity' , 'full stack cybersecurity' , 'cybersecurity'),
+(109 , 'c' , 'c language' , 'c'),
+(108 , 'c++' , 'C++ language' , 'language');
+
+create table if not exists course4 (
+course_id int ,
+course_name varchar(50),
+course_desc varchar(60),
+course_tag varchar(50),
+index(course_desc,course_name,course_tag)
+);
+
+insert into course4 values(101 , 'fsda' , 'full stack data analytics' , 'Analytics'),
+(102 , 'fsds' , 'full stack data analytics' , 'Analytics'),
+(103 , 'fsds' , 'full stack data science' , 'DS'),
+(104 , 'big data' , 'full stack big data' , 'BD'),
+(105 , 'mern' , 'web dev' , 'mern'),
+(106 , 'blockchain' , 'full stack blockchain' , 'BC'),
+(101 , 'java' , 'full stack java' , 'java'),
+(102 , 'testing' , 'full testing ' , 'testing '),
+(105 , 'cybersecurity' , 'full stack cybersecurity' , 'cybersecurity'),
+(109 , 'c' , 'c language' , 'c'),
+(108 , 'c++' , 'C++ language' , 'language');
+
+show index from course4;
+
+
